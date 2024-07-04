b6140637e56324dfae4f20d0cd1c1078953a8e70
LesegoLSG <lesegomhlongo78@gmail.com>
2024-07-04 03:25:48 -0500

    Added a header with a logo

--- convertor/src/Pages/MainPage.jsx
+++ convertor/src/Pages/MainPage.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import teamMember1 from "../assets/teamMember1.png";
-import Logo1 from "../assets/Logo1.png";
 import textSpeech from "../assets/textSpeech.png";
 import speechText from "../assets/speechText.png";
+import Header from "../Components/Header";
 
 const MainPage = () => {
   const navigate = useNavigate();
   return (
     <section className="w-full min-h-screen  bg-gradient-to-r from-darkColor to-secondary text-white">
-      <header className="w-full h-12  flex justify-start items-center px-4">
-        <img src={Logo1} alt="Logo" className="w-32" />
-      </header>
-      <div className="w-full min-h-screen flex flex-col justify-center items-center ">
+      <Header />
+      <div className="w-full min-h-screen flex flex-col justify-center items-center mt-16 md:m-0 ">
         <header className="text-center mb-12 px-4">
           <h1 className="text-4xl md:text-5xl font-bold mb-4">
             Voice-Text Converter
@@ -22,14 +20,6 @@ const MainPage = () => {
           </h2>
         </header>
         <div className="flex flex-col md:flex-row justify-center items-center gap-6 px-4">
-          {/* Voice to Text */}
-          <div
-            className="w-full md:w-[300px] h-[300px] flex flex-col justify-center items-center bg-action rounded-lg shadow-lg cursor-pointer transform transition-transform hover:scale-105 p-4"
-            onClick={() => navigate("/voicetotext")}
-          >
-            <img src={speechText} alt="anim1" className="h-40" />
-            <h1 className="text-2xl font-bold mt-4">Voice to Text</h1>
-          </div>
           {/* Text to Voice */}
           <div
             className="w-full md:w-[300px] h-[300px] flex flex-col justify-center items-center bg-action rounded-lg shadow-lg cursor-pointer transform transition-transform hover:scale-105 p-4"
@@ -37,6 +27,14 @@ const MainPage = () => {
           >
             <img src={textSpeech} alt="anim2" className="h-40" />
             <h1 className="text-2xl font-bold mt-4">Text to Voice</h1>
+          </div>
+          {/* Voice to Text */}
+          <div
+            className="w-full md:w-[300px] h-[300px] flex flex-col justify-center items-center bg-action rounded-lg shadow-lg cursor-pointer transform transition-transform hover:scale-105 p-4"
+            onClick={() => navigate("/voicetotext")}
+          >
+            <img src={speechText} alt="anim1" className="h-40" />
+            <h1 className="text-2xl font-bold mt-4">Voice to Text</h1>
           </div>
         </div>
       </div>
