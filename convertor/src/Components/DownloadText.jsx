02d37b062967a6a5d4cea436601ce4a52b933dd4
LesegoLSG <lesegomhlongo78@gmail.com>
2024-07-04 03:25:17 -0500

    Implemented text newline

--- convertor/src/Components/DownloadText.jsx
+++ convertor/src/Components/DownloadText.jsx
@@ -4,17 +4,27 @@ import { Document, Packer, Paragraph, TextRun } from "docx";
 import { saveAs } from "file-saver";
 
 const DownloadText = ({ text }) => {
-  //Download in pdf
+  // Download in PDF
   const downloadAsPDF = () => {
     const doc = new jsPDF();
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const margin = 10;
+    const maxLineWidth = pageWidth - margin * 2;
     const lines = text.split("\n");
-    lines.forEach((line, index) => {
-      doc.text(line, 10, 10 + index * 10);
+    let verticalOffset = margin;
+
+    lines.forEach((line) => {
+      const splittedText = doc.splitTextToSize(line, maxLineWidth);
+      splittedText.forEach((txt) => {
+        doc.text(txt, margin, verticalOffset);
+        verticalOffset += 10;
+      });
     });
-    doc.save("document.pdf");
+
+    doc.save("less-ego-converter.pdf");
   };
 
-  //   Download as word
+  // Download as Word
   const downloadAsWord = async () => {
     const doc = new Document({
       sections: [
@@ -31,7 +41,7 @@ const DownloadText = ({ text }) => {
 
     try {
       const blob = await Packer.toBlob(doc);
-      saveAs(blob, "document.docx");
+      saveAs(blob, "less-ego-converter.docx");
     } catch (error) {
       console.error("Error creating Word document:", error);
     }
@@ -41,13 +51,13 @@ const DownloadText = ({ text }) => {
     <div className="w-full flex flex-col md:flex-row justify-center items-center ">
       <button
         onClick={downloadAsPDF}
-        className="bg-blue-600 text-white rounded-lg px-4 py-2"
+        className="bg-action text-white rounded-lg px-6 py-3 hover:scale-105 transform transition-transform m-1"
       >
         Download as PDF
       </button>
       <button
         onClick={downloadAsWord}
-        className="bg-blue-600 text-white rounded-lg px-4 py-2"
+        className="bg-action text-white rounded-lg px-6 py-3 hover:scale-105 transform transition-transform m-1"
       >
         Download as Word
       </button>
